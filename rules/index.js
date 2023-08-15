75ed660544772bf547b6f9a831e0fdcf143f5caf
Daniil Suvorov <severecloud@gmail.com>
2023-08-15 09:23:59 +0300

    feat: add no-computed-hit-css-modules (#47)
    Добавляем линтер на проверку вычисляемого обращения к css modules

--- rules/index.js
+++ rules/index.js
@@ -1,3 +1,4 @@
 module.exports = {
   'no-object-expression-in-arguments': require('./no-object-expression-in-arguments'),
+  'no-computed-hit-css-modules': require('./no-computed-hit-css-modules'),
 };
